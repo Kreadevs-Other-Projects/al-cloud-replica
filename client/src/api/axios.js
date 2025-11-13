85bc317e5b3b1b66b35fe0c9529d44f6d901a65a
saadkhan2211 <saadkhan.og01@gmail.com>
2025-11-13 05:27:33 +0500

    fixes

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://cloudcare-dc8k.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
--- server/config/db.js
+++ server/config/db.js
@@ -2,9 +2,13 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      dbName: "AL-Cloud",
-    });
+    const conn = await mongoose.connect(
+      process.env.MONGO_URI ||
+        "mongodb+srv://nighthawkog01:tZglaSklRFU5KJjT@real-estate.jwjphd5.mongodb.net/?retryWrites=true&w=majority&appName=Real-Estate",
+      {
+        dbName: "AL-Cloud",
+      }
+    );
     console.log(`MongoDB connected: ${conn.connection.db.databaseName}`);
   } catch (err) {
     console.error("MongoDB error:", err);
